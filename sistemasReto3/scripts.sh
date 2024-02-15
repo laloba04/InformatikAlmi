d438230c05e67f62dafdba76642fa87cd36a27ef
maria <104345228+laloba04@users.noreply.github.com>
2024-02-15 10:36:31 +0100

    Update scripts.sh
    color

--- sistemasReto3/scripts.sh
+++ sistemasReto3/scripts.sh
@@ -2,19 +2,19 @@
 
 while true; do
     clear
-    echo "================================================"
-    echo "================ InformatikAlmi ================"
-    echo "================================================"
-    echo "Seleccione una opción:"
-    echo "1. Copia de seguridad" 
-    echo "2. Dime que programa quieres instalar?" 
-    echo "3. Actualizaciones del sistema" 
-    echo "4. Monitorización del sistema" 
-    echo "5. Mostrar espacio en disco"
-    echo "6. Ver archivos en el directorio actual" 
-    echo "7. Buscar archivos en el sistema" 
-    echo "8. Mostrar el ultimo usuario creado" 
-    echo "9. Salir" 
+    echo -e "\e[1;36m================================================\e[0m"
+    echo -e "\e[1;36m================ InformatikAlmi ================\e[0m"
+    echo -e "\e[1;36m================================================\e[0m"
+    echo -e "\e[1;33mSeleccione una opción:\e[0m"
+    echo -e "\e[1;32m1. Copia de seguridad\e[0m" 
+    echo -e "\e[1;32m2. Dime qué programa quieres instalar?\e[0m" 
+    echo -e "\e[1;32m3. Actualizaciones del sistema\e[0m" 
+    echo -e "\e[1;32m4. Monitorización del sistema\e[0m" 
+    echo -e "\e[1;32m5. Mostrar espacio en disco\e[0m"
+    echo -e "\e[1;32m6. Ver archivos en el directorio actual\e[0m" 
+    echo -e "\e[1;32m7. Buscar archivos en el sistema\e[0m" 
+    echo -e "\e[1;32m8. Mostrar el último usuario creado\e[0m" 
+    echo -e "\e[1;31m9. Salir\e[0m" 
 
     read -p "Ingrese el número de la opción: " opcion
 
@@ -22,7 +22,7 @@ while true; do
 
         1)
             # Copia de seguridad
-            echo "Iniciando copia de seguridad..."
+            echo -e "\e[1;34mIniciando copia de seguridad...\e[0m"
             read -p "¿De qué usuario quieres hacer el backup?: " backup
             read -p "¿Dónde quieres hacer el backup?: " ruta
             fecha_backup=$(date +%y-%m-%d)
@@ -34,71 +34,71 @@ while true; do
             fi
 
             sudo cp -r "/home/$backup" "$backup_dir"
-            echo "Copia de seguridad completada en: $backup_dir"
+            echo -e "\e[1;32mCopia de seguridad completada en: $backup_dir\e[0m"
             ;;
 
         2)
             # Programa a instalar
-            read -p "¿que programa quieres instalar?: " instalar
+            read -p "¿Qué programa quieres instalar?: " instalar
             sudo apt install $instalar
             ;;
 
         3)
             # Actualizaciones del sistema
-            echo "Buscando actualizaciones..."
+            echo -e "\e[1;34mBuscando actualizaciones...\e[0m"
             sudo apt update && sudo apt upgrade -y
-            echo "Actualización completada."
+            echo -e "\e[1;32mActualización completada.\e[0m"
             ;;
 
         4)
             # Monitorización del sistema
-            echo "Mostrando uso del sistema:"
-            echo "Uso de CPU:"
+            echo -e "\e[1;34mMostrando uso del sistema:\e[0m"
+            echo -e "\e[1;32mUso de CPU:\e[0m"
             top -b -n 1 | grep "Cpu(s)"
-            echo "Uso de Memoria:"
+            echo -e "\e[1;32mUso de Memoria:\e[0m"
             free -m
-            echo "Uso de Almacenamiento:"
+            echo -e "\e[1;32mUso de Almacenamiento:\e[0m"
             df -h
             ;;
 
         5)
             # Mostrar espacio en disco
-            echo "Mostrando espacio en disco:"
+            echo -e "\e[1;34mMostrando espacio en disco:\e[0m"
             df -h
             ;;
 
         6)
             # Ver archivos en el directorio actual
-            echo "Archivos en el directorio actual:"
+            echo -e "\e[1;34mArchivos en el directorio actual:\e[0m"
             ls
             ;;
 
         7)
             # Buscar archivos en el sistema
             read -p "Ingrese el nombre del archivo a buscar: " nombre_archivo
-            echo "Buscando archivos con el nombre '$nombre_archivo'..."
+            echo -e "\e[1;34mBuscando archivos con el nombre '$nombre_archivo'...\e[0m"
             
             # Utiliza el comando find para buscar archivos con el nombre proporcionado
             find / -type f -name "$nombre_archivo" 2>/dev/null
             
-            echo "Búsqueda completada."
+            echo -e "\e[1;32mBúsqueda completada.\e[0m"
             ;;
 
         8)
             # Ultimo usuario creado
-            echo A continuacion mostraremos el ultimo usuario creado:
+            echo -e "\e[1;34mA continuación mostraremos el último usuario creado:\e[0m"
             tail -n1 /etc/passwd | cut -d ":" -f1
             ;;
 
         9)
             # Salir del script
-            echo "¡Hasta luego!"
+            echo -e "\e[1;31m¡Hasta luego!\e[0m"
             exit
             ;;
 
         *)
             # Opción no válida
-            echo "Opción no válida. Inténtalo de nuevo."
+            echo -e "\e[1;31mOpción no válida. Inténtalo de nuevo.\e[0m"
             ;;
     esac
 
